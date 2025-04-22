c7ab4a399717fc41837fa680641269260d68e87b
SridharH461 <sridharsmart461@gmail.com>
2025-04-22 11:53:26 +0530

    first commit

---
+++ src/main/java/com/git/Sample.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class Sample {
+
+	
+		
+		private void Task1() {
+System.out.println("1");
+		}
+	
+		private void Task2() {
+			System.out.println("2");
+					}
+		private void Task3() {
+			System.out.println("3");
+					}
+}
